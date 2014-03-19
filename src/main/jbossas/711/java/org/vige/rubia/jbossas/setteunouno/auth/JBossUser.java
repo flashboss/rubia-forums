8c502b838221c1c0b749dcaba94f62a760b2e39f
Luca Stancapiano <luca.stancapiano@eng.it>
2014-03-19 21:28:00 +0100

    added new application servers

---
+++ src/main/jbossas/711/java/org/vige/rubia/jbossas/setteunouno/auth/JBossUser.java
@@ -0,0 +1,33 @@
+package org.vige.rubia.jbossas.setteunouno.auth;
+
+import org.vige.rubia.auth.User;
+
+public class JBossUser implements User {
+
+	private String id;
+	private String userName;
+	
+	public JBossUser(org.picketlink.idm.model.User user) {
+		this.userName = user.getKey();
+		this.id = user.getId();
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(String id) {
+		this.id = id;
+		
+	}
+}
---
+++ src/main/jbossas/711/java/org/vige/rubia/jbossas/setteunouno/auth/JBossUserProfileModule.java
@@ -0,0 +1,40 @@
+package org.vige.rubia.jbossas.setteunouno.auth;
+
+import java.util.Map;
+
+import javax.inject.Named;
+
+import org.vige.rubia.auth.ProfileInfo;
+import org.vige.rubia.auth.User;
+import org.vige.rubia.auth.UserProfileModule;
+
+@Named("userProfileModule")
+public class JBossUserProfileModule implements UserProfileModule {
+
+	@Override
+	public Object getProperty(User arg0, String arg1) throws IllegalArgumentException {
+		return null;
+	}
+
+	@Override
+	public void setProperty(User arg0, String arg1, Object arg2) throws IllegalArgumentException {
+		
+	}
+
+	@Override
+	public Map<Object, Object> getProperties(User arg0) throws IllegalArgumentException {
+		return null;
+	}
+
+	@Override
+	public ProfileInfo getProfileInfo() {
+		return null;
+	}
+
+	@Override
+	public Object getPropertyFromId(String arg0, String arg1) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
