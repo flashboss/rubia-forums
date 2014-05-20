2a57095285444bbefeee4c9db77612a5d9e66570
Luca Stancapiano <luca.stancapiano@eng.it>
2014-05-20 11:06:47 +0200

    create category test

--- src/test/java/org/vige/rubia/selenium/adminpanel/action/CreateCategory.java
+++ src/test/java/org/vige/rubia/selenium/adminpanel/action/CreateCategory.java
@@ -1,23 +1,33 @@
 package org.vige.rubia.selenium.adminpanel.action;
 
+import static org.jboss.test.selenium.locator.LocatorFactory.id;
 import static org.jboss.test.selenium.locator.LocatorFactory.jq;
+import static org.jboss.test.selenium.locator.LocatorFactory.link;
 
 import java.util.ResourceBundle;
 
 import org.jboss.test.selenium.framework.AjaxSelenium;
+import org.jboss.test.selenium.locator.IdLocator;
 import org.jboss.test.selenium.locator.JQueryLocator;
+import org.jboss.test.selenium.locator.LinkLocator;
 
 public class CreateCategory {
 
-	public static final JQueryLocator ADMIN_PANEL_LINK = jq("[value='"
-			+ ResourceBundle.getBundle("ResourceJSF").getString("Admin_panel")
-			+ "']");
-	public static final JQueryLocator CREATE_CATEGORY_LINK = jq("[href^='/magazzino/search/search_receipt']");
-	public static final JQueryLocator RESULT_CREATE_CATEGORY = jq("[id='receiptSelectionForm:noReceiptMsg']");
+	public static final LinkLocator ADMIN_PANEL_LINK = link(ResourceBundle
+			.getBundle("ResourceJSF").getString("Admin_panel"));
+	public static final JQueryLocator CREATE_CATEGORY_LINK = jq("[name='newCategory']");
+	public static final IdLocator CREATE_CATEGORY_TITLE_INPUT_TEXT = id("addCategoryForm:Category");
+	public static final IdLocator CREATE_CATEGORY_BUTTON = id("addCategoryForm:editinline");
+	public static final JQueryLocator RESULT_CREATE_CATEGORY = jq("[class='successtext']");
 
 	public static String createCategory(AjaxSelenium selenium,
-			String categoryName) {
+			String categoryTitle) {
 		selenium.click(ADMIN_PANEL_LINK);
+		selenium.waitForPageToLoad();
+		selenium.click(CREATE_CATEGORY_LINK);
+		selenium.waitForPageToLoad();
+		selenium.type(CREATE_CATEGORY_TITLE_INPUT_TEXT, categoryTitle);
+		selenium.click(id("addCategoryForm:editinline"));
 		selenium.waitForPageToLoad();
 		String message = selenium.getText(RESULT_CREATE_CATEGORY);
 		return message;
--- src/test/java/org/vige/rubia/selenium/adminpanel/action/CreateForum.java
+++ src/test/java/org/vige/rubia/selenium/adminpanel/action/CreateForum.java
@@ -1,17 +1,25 @@
 package org.vige.rubia.selenium.adminpanel.action;
 
 import static org.jboss.test.selenium.locator.LocatorFactory.jq;
+import static org.jboss.test.selenium.locator.LocatorFactory.link;
+
+import java.util.ResourceBundle;
 
 import org.jboss.test.selenium.framework.AjaxSelenium;
 import org.jboss.test.selenium.locator.JQueryLocator;
+import org.jboss.test.selenium.locator.LinkLocator;
 
 public class CreateForum {
 
-	public static final JQueryLocator CREATE_FORUM_LINK = jq("[href^='/magazzino/search/search_receipt']");
+	public static final LinkLocator ADMIN_PANEL_LINK = link(ResourceBundle
+			.getBundle("ResourceJSF").getString("Admin_panel"));
+	public static final JQueryLocator CREATE_FORUM_LINK = jq("[name='newForum']");
 	public static final JQueryLocator RESULT_CREATE_FORUM = jq("[id='receiptSelectionForm:noReceiptMsg']");
 
 	public static String createForum(AjaxSelenium selenium, String forumName,
 			String forumDescription, String categoryTitle) {
+		selenium.click(ADMIN_PANEL_LINK);
+		selenium.waitForPageToLoad();
 		selenium.click(CREATE_FORUM_LINK);
 		selenium.waitForPageToLoad();
 		String message = selenium.getText(RESULT_CREATE_FORUM);
