70ead27081ebe85a011b68c940041dc345fdef57
Luca Stancapiano <luca.stancapiano@eng.it>
2014-05-20 18:20:18 +0200

    support for gatein 3.7.0 and 3.8.1

---
+++ src/main/gatein/381/portlet/java/org/vige/rubia/gatein/treottouno/portlet/auth/GateinUserModule.java
@@ -0,0 +1,123 @@
+package org.vige.rubia.gatein.treottouno.portlet.auth;
+
+import static javax.faces.context.FacesContext.getCurrentInstance;
+import static org.exoplatform.container.ExoContainerContext.getCurrentContainer;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.inject.Named;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.portal.config.UserACL;
+import org.exoplatform.services.organization.Group;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.UserHandler;
+import org.vige.rubia.auth.User;
+import org.vige.rubia.auth.UserModule;
+
+@Named("userModule")
+public class GateinUserModule implements UserModule, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4471050522969443933L;
+
+	private UserHandler userHandler;
+
+	@Override
+	public User findUserByUserName(String arg0) throws IllegalArgumentException {
+		User user = null;
+		try {
+			if (userHandler == null)
+				userHandler = getUserHandler();
+			org.exoplatform.services.organization.User newUser = getUserHandler()
+					.findUserByName(arg0);
+			user = new GateinUser(newUser);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	/**
+	 * DOCUMENT_ME
+	 * 
+	 * @return DOCUMENT_ME
+	 */
+	@Override
+	public User findUserById(String arg0) {
+		User user = null;
+		try {
+			org.exoplatform.services.organization.User newUser = getUserHandler()
+					.findUserByName(arg0);
+			user = new GateinUser(newUser);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	private UserHandler getUserHandler() {
+		if (userHandler == null) {
+			ExoContainer container = PortalContainer.getInstance();
+			OrganizationService organizationService = (OrganizationService) container
+					.getComponentInstanceOfType(OrganizationService.class);
+			userHandler = organizationService.getUserHandler();
+		}
+		return userHandler;
+	}
+
+	@Override
+	public boolean isGuest() {
+		Collection<?> groups = null;
+		OrganizationService orgService = getOrganizationService();
+		try {
+			String userId = getCurrentInstance().getExternalContext()
+					.getRemoteUser();
+			if (userId == null)
+				return true;
+			groups = orgService.getGroupHandler().findGroupsOfUser(userId);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		if (groups != null)
+			if (groups.size() == 1)
+				return ((Group) groups.toArray()[0]).getGroupName().equals(
+						getGuestGroupName());
+		return false;
+	}
+
+	private OrganizationService getOrganizationService() {
+		ExoContainer container = getCurrentContainer();
+		OrganizationService orgService = (OrganizationService) container
+				.getComponentInstanceOfType(OrganizationService.class);
+		return orgService;
+	}
+
+	private static UserACL getUserACL() {
+		ExoContainer container = getCurrentContainer();
+		UserACL acl = (UserACL) container
+				.getComponentInstanceOfType(UserACL.class);
+		return acl;
+	}
+
+	private String getGuestGroupName() {
+		OrganizationService orgService = getOrganizationService();
+		Group group = null;
+		try {
+			group = orgService.getGroupHandler().findGroupById(
+					getUserACL().getGuestsGroup());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (group != null)
+			return group.getGroupName();
+		return "";
+	}
+}
---
+++ src/main/gatein/381/web/java/org/vige/rubia/gatein/treottouno/web/auth/GateinUserProfileModule.java
@@ -0,0 +1,56 @@
+package org.vige.rubia.gatein.treottouno.web.auth;
+
+import java.util.Map;
+
+import javax.inject.Named;
+
+import org.exoplatform.container.ExoContainer;
+import org.exoplatform.container.PortalContainer;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.organization.UserProfileHandler;
+import org.vige.rubia.auth.ProfileInfo;
+import org.vige.rubia.auth.User;
+import org.vige.rubia.auth.UserProfileModule;
+
+@Named("userProfileModule")
+public class GateinUserProfileModule implements UserProfileModule {
+
+	private UserProfileHandler userProfileHandler;
+
+	@Override
+	public Object getProperty(User arg0, String arg1) throws IllegalArgumentException {
+		getUserHandler();
+		return null;
+	}
+
+	@Override
+	public void setProperty(User arg0, String arg1, Object arg2) throws IllegalArgumentException {
+
+	}
+
+	@Override
+	public Map<Object, Object> getProperties(User arg0) throws IllegalArgumentException {
+		return null;
+	}
+
+	@Override
+	public ProfileInfo getProfileInfo() {
+		return null;
+	}
+
+	private UserProfileHandler getUserHandler() {
+		if (userProfileHandler == null) {
+			ExoContainer container = PortalContainer.getInstance();
+			OrganizationService organizationService = (OrganizationService) container.getComponentInstanceOfType(OrganizationService.class);
+			userProfileHandler = organizationService.getUserProfileHandler();
+		}
+		return userProfileHandler;
+	}
+
+	@Override
+	public Object getPropertyFromId(String arg0, String arg1) throws IllegalArgumentException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
