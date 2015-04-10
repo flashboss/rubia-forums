55c814f899fbfaa0c880984e9552396413d42785
Luca Stancapiano <luca.stancapiano@eng.it>
2015-04-10 22:43:25 +0200

    added support for new platforms

---
+++ src/main/gatein/382/web/java/org/vige/rubia/gatein/treottodue/web/auth/GateinUser.java
@@ -0,0 +1,32 @@
+package org.vige.rubia.gatein.treottodue.web.auth;
+
+import org.vige.rubia.auth.User;
+
+public class GateinUser implements User {
+
+	private String id;
+	private String userName;
+
+	public GateinUser(org.exoplatform.services.organization.User user) {
+		this.userName = user.getUserName();
+		this.id = user.getUserName();
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public void setId(String id) {
+		this.id = id;
+	}
+}
---
+++ src/main/jbossas/713/java/org/vige/rubia/jbossas/setteunotre/auth/JBossACLProvider.java
@@ -0,0 +1,109 @@
+package org.vige.rubia.jbossas.setteunotre.auth;
+
+import static org.jboss.security.acl.BasicACLPermission.READ;
+
+import javax.ejb.EJBContext;
+import javax.ejb.Stateless;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.vige.rubia.auth.ForumsACLProvider;
+import org.vige.rubia.auth.ForumsACLResource;
+import org.vige.rubia.auth.JSFActionContext;
+import org.vige.rubia.auth.JSFUIContext;
+import org.vige.rubia.auth.SecurityContext;
+import org.jboss.security.acl.ACLProvider;
+import org.jboss.security.acl.ACLProviderImpl;
+import org.jboss.security.authorization.AuthorizationException;
+import org.jboss.security.identity.Identity;
+import org.jboss.security.identity.plugins.SimpleIdentity;
+
+@Named("forumsACLProvider")
+@Stateless
+public class JBossACLProvider implements ForumsACLProvider {
+
+	private static final long serialVersionUID = -5490482161183021121L;
+
+	@PersistenceContext(unitName = "forums")
+	private EntityManager em;
+
+	@javax.annotation.Resource
+	private EJBContext ejbContext;
+
+	private ACLProvider provider = new ACLProviderImpl();
+
+	@Override
+	public boolean hasAccess(SecurityContext context) {
+		if (context instanceof JSFUIContext)
+			return hasAccess((JSFUIContext) context);
+		else
+			return hasAccess((JSFActionContext) context);
+	}
+
+	public boolean hasAccess(JSFUIContext context) {
+		final String aclContextStr = context.getFragment();
+		provider.setPersistenceStrategy(new ForumsJPAPersistenceStrategy(em));
+		ForumsACLResource resource = null;
+		resource = em.find(ForumsACLResource.class, aclContextStr);
+		if (resource == null)
+			resource = new ForumsACLResource(aclContextStr);
+		String roleId = "";
+		if (ejbContext.getCallerPrincipal().getName().equals("anonymous"))
+			roleId = "guests";
+		else if (ejbContext.isCallerInRole("admins")
+				|| ejbContext.isCallerInRole("admin"))
+			return true;
+		else if (ejbContext.isCallerInRole("users")
+				|| ejbContext.isCallerInRole("user"))
+			roleId = "users";
+		Identity identity = new SimpleIdentity(roleId);
+		boolean authorized = false;
+		try {
+			authorized = provider.isAccessGranted(resource, identity, READ);
+			if (authorized) {
+				resource.add("runtimeInfo", context.getContextData());
+				resource.add("identity", context.getIdentity());
+				authorized = resource.evaluate();
+			}
+		} catch (AuthorizationException e) {
+			e.printStackTrace();
+		}
+		return authorized;
+	}
+
+	public boolean hasAccess(JSFActionContext context) {
+		String className = context.getManagedBean().getClass().getName();
+		final String aclContextStr = className.substring(0,
+				className.indexOf("$Proxy$_$$_WeldSubclass"))
+				+ ":" + context.getBusinessAction().getName();
+		provider.setPersistenceStrategy(new ForumsJPAPersistenceStrategy(em));
+		ForumsACLResource resource = null;
+		resource = em.find(ForumsACLResource.class, aclContextStr);
+		if (resource == null)
+			resource = new ForumsACLResource(aclContextStr);
+		String roleId = "";
+		if (ejbContext.getCallerPrincipal().getName().equals("anonymous"))
+			roleId = "guests";
+		else if (ejbContext.isCallerInRole("admins")
+				|| ejbContext.isCallerInRole("admin"))
+			return true;
+		else if (ejbContext.isCallerInRole("users")
+				|| ejbContext.isCallerInRole("user"))
+			roleId = "users";
+		Identity identity = new SimpleIdentity(roleId);
+		boolean authorized = false;
+		try {
+			authorized = provider.isAccessGranted(resource, identity, READ);
+			if (authorized) {
+				resource.add("runtimeInfo", new Object[]{context.getManagedBean()});
+				resource.add("identity", context.getIdentity());
+				authorized = resource.evaluate();
+			}
+		} catch (AuthorizationException e) {
+			e.printStackTrace();
+		}
+		return authorized;
+	}
+
+}
