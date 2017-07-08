409bd51262502218efbcd809a8dd9612563fcbc7
flashboss <luca.stancapiano@vige.it>
2017-07-08 10:07:50 +0200

    remove forums tests

--- rubia-forums-jsf/src/test/java/it/vige/rubia/selenium/adminpanel/action/RemoveForum.java
+++ rubia-forums-jsf/src/test/java/it/vige/rubia/selenium/adminpanel/action/RemoveForum.java
@@ -20,35 +20,32 @@ import static java.util.ResourceBundle.getBundle;
 import static org.openqa.selenium.By.className;
 import static org.openqa.selenium.By.linkText;
 import static org.openqa.selenium.By.xpath;
-import it.vige.rubia.model.Forum;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+
+import it.vige.rubia.model.Forum;
 
 public class RemoveForum {
 
-	public static final String ADMIN_PANEL_LINK = getBundle("ResourceJSF")
-			.getString("Admin_panel");
+	public static final String ADMIN_PANEL_LINK = getBundle("ResourceJSF").getString("Admin_panel");
 	public static final String REMOVE_FORUM_LINK = "buttonMed";
 	public static final String SELECT_TYPE = "//form/select";
 	public static final String RESULT_REMOVE_FORUM = "successtext";
 
-	public static String removeForum(WebDriver driver, Forum forum,
-			String removeType) {
-		WebElement adminPanelLink = driver
-				.findElement(linkText(ADMIN_PANEL_LINK));
+	public static String removeForum(WebDriver driver, Forum forum, String removeType) {
+		WebElement adminPanelLink = driver.findElement(linkText(ADMIN_PANEL_LINK));
 		adminPanelLink.click();
 		WebElement removeForum = driver
-				.findElement(xpath("//tr[td/strong/text()='" + forum.getName()
-						+ "']/td[2]/form/ul/li[3]/a/img"));
+				.findElement(xpath("//tr[td/strong/text()='" + forum.getName() + "']/td[2]/form/ul/li[3]/a/img"));
 		removeForum.click();
 		WebElement categoryOption = driver.findElement(xpath(SELECT_TYPE));
-		categoryOption.sendKeys(removeType);
-		WebElement removeForumLink = driver
-				.findElement(className(REMOVE_FORUM_LINK));
+		Select select = new Select(categoryOption);
+		select.selectByVisibleText(removeType);
+		WebElement removeForumLink = driver.findElement(className(REMOVE_FORUM_LINK));
 		removeForumLink.click();
-		WebElement resultRemoveForum = driver
-				.findElement(className(RESULT_REMOVE_FORUM));
+		WebElement resultRemoveForum = driver.findElement(className(RESULT_REMOVE_FORUM));
 		String message = resultRemoveForum.getText();
 		return message;
 	}
