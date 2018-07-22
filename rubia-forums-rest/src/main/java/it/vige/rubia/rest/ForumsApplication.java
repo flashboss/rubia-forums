98602b98e38f7b4dd2ac1fa8302cc7b131aea861
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-07-22 16:44:05 +0200

    rest tests

--- rubia-forums-rest/src/main/java/it/vige/rubia/rest/ForumsApplication.java
+++ rubia-forums-rest/src/main/java/it/vige/rubia/rest/ForumsApplication.java
@@ -3,6 +3,6 @@ package it.vige.rubia.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("rest")
+@ApplicationPath("services")
 public class ForumsApplication extends Application {
 }
