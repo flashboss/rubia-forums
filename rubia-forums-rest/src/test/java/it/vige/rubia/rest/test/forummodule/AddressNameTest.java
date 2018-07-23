141d7acfbf5bb2eaab9e077a2ce81a07a0a43281
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-07-23 18:15:46 +0200

    junit upgrade

--- rubia-forums-rest/src/test/java/it/vige/rubia/rest/test/forummodule/AddressNameTest.java
+++ rubia-forums-rest/src/test/java/it/vige/rubia/rest/test/forummodule/AddressNameTest.java
@@ -11,28 +11,28 @@ import org.junit.jupiter.api.Test;
 
 public class AddressNameTest {
 
-	private Client client = newClient();
-	private String url = "http://localhost:8080/rubia-forums-rest/services/forums/";
+	private final static Client client = newClient();
+	private final static String url = "http://localhost:8080/rubia-forums-rest/services/forums/";
+	private final static String authorization = "Basic cm9vdDpndG4=";
 
 	@Test
 	public void setGuestUserName() {
-		Response response = getResponse(url + "setGuestUserName/bubbusettete");
+		Response response = getResponse(url + "setGuestUserName/bubbusettete", authorization);
 		response.close();
-		response = getResponse(url + "getGuestUserName");
+		response = getResponse(url + "getGuestUserName", authorization);
 		String value = response.readEntity(String.class);
 		response.close();
 		assertEquals("bubbusettete", value, "Guest user name sent");
-		response = getResponse(url + "setGuestUserName/guest");
+		response = getResponse(url + "setGuestUserName/guest", authorization);
 		response.close();
-		response = getResponse(url + "getGuestUserName");
+		response = getResponse(url + "getGuestUserName", authorization);
 		value = response.readEntity(String.class);
 		response.close();
 		assertEquals("guest", value, "Guest user name sent");
 	}
 
-	private Response getResponse(String url) {
+	private Response getResponse(String url, String authorization) {
 		WebTarget target = client.target(url);
-		return target.request().header("Authorization", "Basic cm9vdDpndG4=").get();
-
+		return target.request().header("Authorization", authorization).get();
 	}
 }
--- rubia-forums-rest/src/test/java/it/vige/rubia/rest/test/forummodule/GuestUserTest.java
+++ rubia-forums-rest/src/test/java/it/vige/rubia/rest/test/forummodule/GuestUserTest.java
@@ -11,28 +11,28 @@ import org.junit.jupiter.api.Test;
 
 public class GuestUserTest {
 
-	private Client client = newClient();
-	private String url = "http://localhost:8080/rubia-forums-rest/services/forums/";
+	private final static Client client = newClient();
+	private final static String url = "http://localhost:8080/rubia-forums-rest/services/forums/";
+	private final static String authorization = "Basic cm9vdDpndG4=";
 
 	@Test
 	public void setAddressName() {
-		Response response = getResponse(url + "setFromAddress/bubbusettete2");
+		Response response = getResponse(url + "setFromAddress/bubbusettete2", authorization);
 		response.close();
-		response = getResponse(url + "getFromAddress");
+		response = getResponse(url + "getFromAddress", authorization);
 		String value = response.readEntity(String.class);
 		response.close();
 		assertEquals("bubbusettete2", value, "From address name sent");
-		response = getResponse(url + "setFromAddress/portal@example.com");
+		response = getResponse(url + "setFromAddress/portal@example.com", authorization);
 		response.close();
-		response = getResponse(url + "getFromAddress");
+		response = getResponse(url + "getFromAddress", authorization);
 		value = response.readEntity(String.class);
 		response.close();
 		assertEquals("portal@example.com", value, "From address name sent");
 	}
 
-	private Response getResponse(String url) {
+	private Response getResponse(String url, String authorization) {
 		WebTarget target = client.target(url);
-		return target.request().header("Authorization", "Basic cm9vdDpndG4=").get();
-
+		return target.request().header("Authorization", authorization).get();
 	}
 }
