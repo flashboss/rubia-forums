a53a3d0c0c5c1691277a2a7fc92ec96f5a9b9362
Luca Stancapiano <l.stancapiano@sourcesense.com>
2018-07-23 18:16:09 +0200

    junit upgrade

---
+++ rubia-forums-rest/src/test/java/it/vige/rubia/rest/test/aclprovider/UIAccessTest.java
@@ -0,0 +1,64 @@
+package it.vige.rubia.rest.test.aclprovider;
+
+import static javax.ws.rs.client.ClientBuilder.newClient;
+import static javax.ws.rs.client.Entity.entity;
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import org.junit.jupiter.api.Test;
+import org.picketlink.idm.model.basic.User;
+
+import it.vige.rubia.auth.UIContext;
+import it.vige.rubia.model.Forum;
+import it.vige.rubia.wildfly.auth.JBossUser;
+
+public class UIAccessTest {
+
+	private final static Client client = newClient();
+	private final static String url = "http://localhost:8080/rubia-forums-rest/services/acl/";
+
+	@Test
+	public void uiRootAccess() {
+		UIContext uiContext = new UIContext();
+		uiContext.setFragment("acl://accessAdminTool");
+		Response response = getResponse(url + "hasAccess", "Basic cm9vdDpndG4=", uiContext);
+		boolean value = response.readEntity(Boolean.class);
+		response.close();
+		assertEquals(true, value, "Has ui root access");
+	}
+
+	@Test
+	public void uiDemoAccess() {
+		UIContext uiContext = new UIContext();
+		uiContext.setFragment("acl://accessAdminTool");
+		Response response = getResponse(url + "hasAccess", "Basic ZGVtbzpndG4=", uiContext);
+		boolean value = response.readEntity(Boolean.class);
+		response.close();
+		assertEquals(false, value, "Has not ui demo access");
+	}
+
+	@Test
+	public void uiDemoAccessAddTopic() {
+		UIContext uiContext = new UIContext();
+		uiContext.setFragment("acl://newTopic");
+		User user = new User("demo");
+		uiContext.setIdentity(new JBossUser(user));
+		Forum forum = new Forum();
+		uiContext.setContextData(new Object[] {forum});
+		Response response = getResponse(url + "hasAccess", "Basic ZGVtbzpndG4=", uiContext);
+		boolean value = response.readEntity(Boolean.class);
+		response.close();
+		assertEquals(true, value, "Has ui demo access");
+	}
+
+	private Response getResponse(String url, String authorization, UIContext uiContext) {
+		WebTarget target = client.target(url);
+		Entity<UIContext> uiContextEntity = entity(uiContext, APPLICATION_JSON);
+		return target.request().header("Authorization", authorization).post(uiContextEntity);
+	}
+}
